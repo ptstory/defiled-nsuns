007c8c013bf4d25059e9e6bd1632b8613a733bfb
Defeat3d <koen.deubel@hva.nl>
2020-07-29 01:28:03 +0200

    Show single day program instead of tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:defiled_nsuns/cycle/dayprogram.dart';
 import 'package:defiled_nsuns/cycle/exercise.dart';
+import 'package:defiled_nsuns/cycle/exerciseset.dart';
 import 'package:flutter/material.dart';
 
-const List<DayProgram> programs = const <DayProgram>[
-  DayProgram(title: 'Bench/Overhead Press', exercises: const <Exercise>[
+List<DayProgram> programs = <DayProgram>[
+  DayProgram(title: 'Bench/Overhead Press', exercises: <Exercise>[
     Exercise(
         name: 'Bench Press',
         targetMuscle: TargetMuscle.chest,
-        sets: const <ExerciseSet>[
+        sets: <ExerciseSet>[
           ExerciseSet(weight: 60, repetitions: 8),
           ExerciseSet(weight: 65, repetitions: 6),
           ExerciseSet(weight: 70, repetitions: 1),
@@ -16,7 +17,7 @@ const List<DayProgram> programs = const <DayProgram>[
     Exercise(
         name: 'Overhead Press',
         targetMuscle: TargetMuscle.shoulders,
-        sets: const <ExerciseSet>[
+        sets: <ExerciseSet>[
           ExerciseSet(weight: 20, repetitions: 8),
           ExerciseSet(weight: 30, repetitions: 6),
           ExerciseSet(weight: 35, repetitions: 1),
@@ -30,31 +31,18 @@ class CycleConfigurator extends StatefulWidget {
   _CycleConfiguratorState createState() => _CycleConfiguratorState();
 }
 
-class _CycleConfiguratorState extends State<CycleConfigurator>
-    with SingleTickerProviderStateMixin {
-  TabController _tabController;
+class _CycleConfiguratorState extends State<CycleConfigurator> {
   String _title = 'Loading...';
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: programs.length, vsync: this);
     _title = programs[0].title;
   }
 
   @override
   void dispose() {
-    _tabController.dispose();
     super.dispose();
-  }
-
-  void _nextPage(int delta) {
-    final int newIndex = _tabController.index + delta;
-    if (newIndex < 0 || newIndex >= _tabController.length) return;
-    _tabController.animateTo(newIndex);
-    setState(() {
-      _title = programs[newIndex].title;
-    });
   }
 
   @override
@@ -63,23 +51,14 @@ class _CycleConfiguratorState extends State<CycleConfigurator>
         appBar: AppBar(
             centerTitle: true,
             title: Text(_title),
-            leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () {
-                  _nextPage(-1);
-                }),
             actions: <Widget>[
               IconButton(
-                  icon: const Icon(Icons.arrow_forward),
-                  onPressed: () {
-                    _nextPage(1);
-                  })
+                  icon: const Icon(Icons.check),
+                onPressed: () {
+                  // Save workout and go to next program
+                })
             ]),
-        body: TabBarView(
-            controller: _tabController,
-            children: programs
-                .map((program) => DayProgramWidget(dayProgram: program))
-                .toList()),
+              body: DayProgramWidget(dayProgram: programs[0])
       ));
 }
 
